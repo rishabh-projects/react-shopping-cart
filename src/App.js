76f38e50264989aea9f00db65e6d4ee4f57bc4c6
Rishabh Mewar <rmewar.dev@gmail.com>
2021-06-07 05:00:15 +0530

    Create "Product List" Component

--- src/App.js
+++ src/App.js
@@ -1,114 +1,34 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
-import { signout } from './actions/userActions';
-import PrivateRoute from './components/PrivateRoute';
-import CartScreen from './screens/CartScreen';
-import HomeScreen from './screens/HomeScreen';
-import OrderHistoryScreen from './screens/OrderHistoryScreen';
-import OrderScreen from './screens/OrderScreen';
-import PaymentMethodScreen from './screens/PaymentMethodScreen';
-import PlaceOrderScreen from './screens/PlaceOrderScreen';
-import ProductScreen from './screens/ProductScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ShippingAddressScreen from './screens/ShippingAddressScreen';
-import SigninScreen from './screens/SigninScreen';
+import React from "react";
+import data from "./data.json";
+import Products from "./components/Products";
 
-function App() {
-  const cart = useSelector((state) => state.cart);
-  const { cartItems } = cart;
-
-  const userSignin = useSelector((state) => state.userSignin);
-  const { userInfo } = userSignin;
-  const dispatch = useDispatch();
-  const signoutHandler = () => {
-    dispatch(signout());
-  };
-
-  return (
-    <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <Link className="brand" to="/">
-            amazona
-          </Link>
-        </div>
-        <div>
-          <Link to="/cart">
-            Cart
-            {cartItems.length > 0 && (
-              <span className="badge">{cartItems.length}</span>
-            )}
-          </Link>
-          <Link to="/signin">Sign In</Link>
-          {userInfo ? (
-              <div className="dropdown">
-                <Link to="#">
-                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
-                </Link>
-                <ul className="dropdown-content">
-                  <li>
-                    <Link to="/profile">User Profile</Link>
-                  </li>
-                  <li>
-                    <Link to="/orderhistory">Order History</Link>
-                  </li>
-                  <li>
-                    <Link to="#signout" onClick={signoutHandler}>
-                      Sign Out
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <Link to="/signin">Sign In</Link>
-            )}
-            {userInfo && userInfo.isAdmin && (
-              <div className="dropdown">
-                <Link to="#admin">
-                  Admin <i className="fa fa-caret-down"></i>
-                </Link>
-                <ul className="dropdown-content">
-                  <li>
-                    <Link to="/dashboard">Dashboard</Link>
-                  </li>
-                  <li>
-                    <Link to="/productlist">Products</Link>
-                  </li>
-                  <li>
-                    <Link to="/orderlist">Orders</Link>
-                  </li>
-                  <li>
-                    <Link to="/userlist">Users</Link>
-                  </li>
-                </ul>
-              </div>
-            )}
-        </div>
-      </header>
-      <main>
-        <Route path="/cart/:id?" component={CartScreen}></Route>
-        <Route path="/product/:id" component={ProductScreen}></Route>
-        <Route path="/signin" component={SigninScreen}></Route>
-        <Route path="/register" component={RegisterScreen}></Route>
-        <Route path="/shipping" component={ShippingAddressScreen}></Route>
-        <Route path="/payment" component={PaymentMethodScreen}></Route>
-        <Route path="/placeorder" component={PlaceOrderScreen}></Route>
-        <Route path="/order/:id" component={OrderScreen}></Route>
-        <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
-        <Route path="/profile" component={ProfileScreen}></Route>
-        <PrivateRoute
-            path="/profile"
-            component={ProfileScreen}
-          ></PrivateRoute>
-        <Route path="/" component={HomeScreen} exact></Route>
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <footer>All right is reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
